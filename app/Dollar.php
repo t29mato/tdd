d3e443398c75c94d85948f71f7d77afa740a1e89
mato <tomoya.matou@gmail.com>
2018-10-27 11:46:42 +0900

    finish chapter.2

---
+++ app/Dollar.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dollar extends Model
+{
+
+    public $amount;
+
+    public function __construct(int $amount) {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier) {
+        return new Dollar($this->amount * $multiplier);
+    }
+}
+
--- tests/Unit/MoneyModelTest.php
+++ tests/Unit/MoneyModelTest.php
@@ -5,18 +5,25 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\MoneyModel;
+use App\Dollar;
 
 class MoneyModelTest extends TestCase
 {
     /**
      * 1. $5 * 2 = %$10
      * 2. $5 + 10 CHF = $10 (レートが2:1の場合)
-     * 
+     *
+     * amountをprivateにする
+     * Dollarの副作用どうする？
+     * Moneyの丸め処理どうする？
      */
-    
+
     public function testMultiplication() {
-        Dollar five = new Dollar(5);
-        five.time(2);
-        assertEquals(10, five.amount);
-    }     
+        $five = new Dollar(5);
+        $product = $five->times(2);
+        $this->assertEquals(10, $product->amount);
+        $product = $five->times(3);
+        $this->assertEquals(15, $product->amount);
+    }
 }
