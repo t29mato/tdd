fba100a66a80663998088dc00d81613d54684ed4
mato <tomoya.matou@gmail.com>
2018-10-28 21:28:56 +0900

    fin chapter.11

--- tests/Unit/MoneyModelTest.php
+++ tests/Unit/MoneyModelTest.php
@@ -13,7 +13,7 @@ use App\Franc;
 class MoneyModelTest extends TestCase
 {
     /**
-     * [o]
+     * [done]
      * $5 * 2 = %$10
      * $5 + 10 CHF = $10 (レートが2:1の場合)
      * equals()
@@ -23,13 +23,15 @@ class MoneyModelTest extends TestCase
      * FrancとDollarを比較する
      * 通貨の概念
      * Dollarの副作用どうする？
-     *
-     * [x]
-     * Moneyの丸め処理どうする？
-     * DollarとFrancの重複
-     * nullとの透過性比較
-     * 他のオブジェクトとの等価性比較
+     * testFrancMultiplicationを削除する?
      * timesの一般化
+     * DollarとFrancの重複
+     *
+     * [ready]
+     * Moneyの丸め処理どうする？
+     * hashCode()
+     * nullとの等価性比較
+     * 他のオブジェクトとの等価性比較
      */
 
     public function testMultiplication() {
@@ -41,23 +43,11 @@ class MoneyModelTest extends TestCase
     public function testEquality() {
         $this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
         $this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
-        $this->assertTrue((Money::franc(5))->equals(Money::franc(5)));
-        $this->assertFalse((Money::franc(5))->equals(Money::franc(6)));
         $this->assertFalse((Money::dollar(5))->equals(Money::franc(5)));
-    }
-
-    public function testFrancMultiplication() {
-        $five = Money::franc(5);
-        $this->assertEquals(Money::franc(10), $five->times(2));
-        $this->assertEquals(Money::franc(15), $five->times(3));
     }
 
     public function testCurrency() {
         $this->assertEquals('USD', Money::dollar(1)->currency());
         $this->assertEquals('CHF', Money::franc(1)->currency());
-    }
-
-    public function testDifferentClassEquality() {
-        $this->assertTrue((new Money(10, 'CHF'))->equals(new Franc(10, 'CHF')));
     }
 }
